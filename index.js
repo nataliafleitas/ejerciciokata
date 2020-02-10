f498776753875e4d841a5138900a106f5b0fd9dc
nataliafleitas <nataliafleitas2298@gmail.com>
2020-02-10 15:23:42 -0300

    Agregamos fecha,año, y dia

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-
-
-function Multiplicando (lista){
- let listaduplicada=[];
-     for(let i=0 ; i < lista.length ;i++){
-     listaduplicada[i]=lista[i] * 2;
+//function Multiplicando (lista){
+ //let listaduplicada=[];
+ //    for(let i=0 ; i < lista.length ;i++){
+ //    listaduplicada[i]=lista[i] * 2;
     
- }
- return listaduplicada;
- }
+ //}
+ //return listaduplicada;
+ //}
+ function fechas (lista){
+    let resultado = 0;
+    let posicion = 0;
+    
+    while (posicion < lista.length) {
+     
+      if(posicion === lista.length -1){
+        resultado = resultado + lista[posicion];
+      }else{
+        resultado = resultado + lista[posicion] + '-';
+      }
+      posicion++;
+    }
+    
+    return resultado;
+    
+   }
+   
+
  
