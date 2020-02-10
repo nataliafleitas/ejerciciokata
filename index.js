24b88b1ba8f634a28d376179523f49d2ee305e48
nataliafleitas <nataliafleitas2298@gmail.com>
2020-02-10 16:51:06 -0300

    Validamos si los objetos son iguales o no, pero no con funcion

--- index.js
+++ index.js
@@ -6,23 +6,40 @@
  //}
  //return listaduplicada;
  //}
- function fechas (lista){
-    let resultado = 0;
-    let posicion = 0;
+ //function fechas (lista){
+   //  let resultado = 0;
+   //  let posicion = 0;
     
-    while (posicion < lista.length) {
+   //  while (posicion < lista.length) {
      
-      if(posicion === lista.length -1){
-        resultado = resultado + lista[posicion];
-      }else{
-        resultado = resultado + lista[posicion] + '-';
-      }
-      posicion++;
-    }
+   //    if(posicion === lista.length -1){
+    //     resultado = resultado + lista[posicion];
+      // }else{
+ //        resultado = resultado + lista[posicion] + '-';
+   //    }
+   //    posicion++;
+   //  }
     
-    return resultado;
+   //  return resultado;
     
-   }
-   
+  //  }
 
- 
+ const persona1 = {
+        nombre:'juanito',
+        apellido: 'messi',
+        localidad: 'nuñez'
+}
+ const persona2 = {
+    nombre:'juanito',
+    apellido: 'messi',
+    localidad: 'nuñez'
+}
+  
+ if(persona1.nombre === persona2.nombre && persona1.apellido === persona2.apellido && persona1.localidad === persona2.localidad){
+    console.log('son iguales');
+ }else{
+    console.log('son falsos');
+ }
+  
+    
+   
