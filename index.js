d4de0f7226a2ac0e086160aea7a6dca270a47b8f
nataliafleitas <nataliafleitas2298@gmail.com>
2020-02-10 14:28:29 -0300

    Multiplicamos por 2 los numeros ingresados del array

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function Multiplicando (lista){
+ let listaduplicada=[];
+     for(let i=0 ; i < lista.length ;i++){
+     listaduplicada[i]=lista[i] * 2;
+    
+ }
+ return listaduplicada;
+ }
+ 
